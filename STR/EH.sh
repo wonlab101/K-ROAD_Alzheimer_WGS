6a76b998a87760cbb0c211ef593a9177231cf7e8
wonlab101 <44426040+wonlab101@users.noreply.github.com>
2025-04-10 21:15:25 +0900

    Delete STR directory

--- STR/EH.sh
+++
@@ -1,7 +0,0 @@
-ExpansionHunter \\
-        --reads sample1.bam \\
-        --reference hg38.fa \\
-        --analysis-mode streaming \\
-        --threads 10 \\
-        --variant-catalog eh.v5_w_gangstr.v13.polymorphic.json \\
-        --output-prefix sample1
